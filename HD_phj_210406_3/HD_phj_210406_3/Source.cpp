3405a604f61eba616c248764667342074a994da9
hjpark9999 <hjpark9999@knou.ac.kr>
2021-04-06 22:07:11 +0900

    소수이하 2자리까지 출력

--- HD_phj_210406_3/HD_phj_210406_3/Source.cpp
+++ HD_phj_210406_3/HD_phj_210406_3/Source.cpp
@@ -7,14 +7,14 @@ void main()
 	float area, circ;
 	int r;
 	
-	
+	printf("지름값을 정수로 입력해 주세요:");
 	scanf_s("%d", &r);
 	fseek(stdin, 0, SEEK_END);
 
 	area = PI * r * r;
 	circ = PI * 2 * r;
 
-	printf("PI=%f이고, 원의넓이=%f, 원의 둘레=%f", PI, area, circ);
+	printf("PI=%.2f, 원의넓이=%.2f, 원의 둘레=%.2f", PI, area, circ);
 	fgetc(stdin);
 
 
